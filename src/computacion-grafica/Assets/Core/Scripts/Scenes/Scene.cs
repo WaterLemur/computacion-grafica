0b5b03ece5490d19c29fe17d25f68fde6d0cb35a
Water_Lemur(); - /mainframe <lemusdwe@gmail.com>
2026-08-09 16:39:02 -0500

    Hotfix

--- src/computacion-grafica/Assets/Core/Scripts/Scenes/Scene.cs
+++ src/computacion-grafica/Assets/Core/Scripts/Scenes/Scene.cs
@@ -12,8 +12,8 @@ public abstract class Scene : MonoBehaviour
         Text sceneInfo = GameObject.Find("SceneInfo").GetComponent<Text>();
         sceneInfo.text = SceneManager.GetActiveScene().name;
         
-        if (string.IsNullOrEmpty(info))
-            sceneInfo.text = sceneInfo.text + " - " + info;
+        if (!string.IsNullOrEmpty(info))
+            sceneInfo.text += " - " + info;
     }
 
     // Update is called once per frame
